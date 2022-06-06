75d13cae677ad525770451d88a14a4895c8d80a4
bichphuongk52 <bichphuongk52@gmail.com>
2022-06-06 19:20:08 +0700

    add

--- model/notify.php
+++ model/notify.php
@@ -16,14 +16,14 @@ class Notify {
     //read db
     public function readInClass($id) {
         $query = "SELECT notify.*, users.class
-FROM notify INNER JOIN users ON notify.class_id = users.id AND notify.class_id = $id";
+FROM notify INNER JOIN users ON notify.class_id = users.user_id AND notify.class_id = $id";
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
         return $stmt;
     }
 
     public function readInHome() {
-        $query = "SELECT * FROM notify WHERE class_id is NULL ORDER BY id DESC";
+        $query = "SELECT * FROM notify WHERE class_id = 0 ORDER BY id DESC";
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
         return $stmt;
