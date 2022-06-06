a5ba3e7f5680ddfab55aec7009504d56293cbc41
bichphuongk52 <bichphuongk52@gmail.com>
2022-06-06 14:58:15 +0700

    'add-3'

--- view/index.php
+++ view/index.php
@@ -11,8 +11,8 @@
 header('Access-Control-Allow-Origin:*');
 header('Content-Type: application/json');
 
-include_once ('../../config/db.php');
-include_once ('../../model/user.php');
+include_once ('../config/db.php');
+include_once ('../model/user.php');
 
 $db = new db();
 $connect = $db->connect();
